9e417747034f4e3ea2e808892a4ae2f8a1a8d533
James Chacon <chacon.james@gmail.com>
2022-09-12 00:24:40 +0000

    Add framework for Chip trait which initially the cpu will implement.

---
+++ chip/src/lib.rs
@@ -0,0 +1,27 @@
+//! `chip` defines the basic interfaces for chips in the 6502 family.
+//! Each chip must implement this in order to be used together in a system
+//! emulation.
+
+use color_eyre::eyre::Result;
+
+/// Chip defines the trait for an 8 bit chip in the 6502 chip era.
+pub trait Chip {
+    /// `tick` is called to run a clock tick.
+    ///
+    /// # Errors
+    /// If calling `tick` results in a lockup/illegal condition an error
+    /// will be returned.
+    fn tick() -> Result<()>;
+
+    /// `done` is called on each `Chip` when all `tick` calls have
+    /// completed. As in real hardware there are no guarentees on ordering
+    /// of `tick` calls so emulation of combinational logic including latches
+    /// should only get updated in `done` to prevent chips on the same
+    /// clock cycle from possibly seeing differing views of data.
+    /// i.e. the Atari 2600 is full of latches which this can come into play.
+    ///
+    /// # Errors
+    /// If calling `done` results in a lockup/illegal condition an error
+    /// will be returned.
+    fn done() -> Result<()>;
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
-//! prelude exports common things for the 6502 such as the cpu and memory modules.
+//! prelude exports common things for the 6502 such as the chip, cpu and memory modules.
 
 /// prelude exports common items for the 6502.
 pub mod prelude {
+    pub use chip::*;
     pub use cpu::*;
     pub use memory::*;
 }
