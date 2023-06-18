c7418364f3183505bbe70529c27a2ba4ad6ab23b
James Chacon <chacon.james@gmail.com>
2023-06-18 22:42:19 +0000

    Run all 3 NMOS variations over the tests cases

--- assemble/src/tests.rs
+++ assemble/src/tests.rs
@@ -23,33 +23,36 @@ macro_rules! assemble_test {
                 fn $name() -> Result<(), Box<dyn Error>> {
                     let a = $assemble_test;
 
-                    // Get the input asm and read it in.
-                    let path = Path::new(env!("CARGO_MANIFEST_DIR")).join("../testdata/").join(a.asm);
-                    println!("trace path: {}", path.display());
-                    let file = File::open(path)?;
-                    let lines = io::BufReader::new(file).lines();
-
                     let bytes = read(Path::new(env!("CARGO_MANIFEST_DIR")).join("../testdata/").join(a.bin))?;
 
                     let mut image: [u8; MAX_SIZE] = [0; MAX_SIZE];
                     for (pos, b) in bytes.iter().enumerate() {
                         image[pos] = *b;
                     }
-                    let asm = parse(Type::NMOS, lines)?;
 
-                    let diff = image
-                        .iter()
-                        .enumerate()
-                        .filter_map(|(pos, b)| {
-                            if asm.bin[pos] == *b {
-                                None
-                            } else {
-                                Some(format!("diff - {pos:04X} - {b:02X} is actually {:02X}", asm.bin[pos]))
-                            }
-                        })
-                        .collect::<Vec<_>>();
-                    assert!(diff.is_empty(),
-                        "Images don't match - Diff at these locations - \n{diff:?}");
+                    // This should be the same for all these CPU's
+                    for t in [Type::NMOS, Type::NMOS6510, Type::Ricoh] {
+                      // Get the input asm and read it in.
+                      let path = Path::new(env!("CARGO_MANIFEST_DIR")).join("../testdata/").join(a.asm);
+
+                      let file = File::open(path)?;
+                      let lines = io::BufReader::new(file).lines();
+                      let asm = parse(t, lines)?;
+
+                      let diff = image
+                          .iter()
+                          .enumerate()
+                          .filter_map(|(pos, b)| {
+                              if asm.bin[pos] == *b {
+                                  None
+                             } else {
+                                  Some(format!("{t:?} - diff - {pos:04X} - {b:02X} is actually {:02X}", asm.bin[pos]))
+                              }
+                          })
+                          .collect::<Vec<_>>();
+                      assert!(diff.is_empty(),
+                          "{t:?} - Images don't match - Diff at these locations - \n{diff:?}");
+                    }
                     Ok(())
                 }
             )*
